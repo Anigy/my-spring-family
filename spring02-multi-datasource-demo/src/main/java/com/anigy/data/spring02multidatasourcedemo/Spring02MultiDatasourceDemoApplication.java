99372acf937399466b9f5f531d53d0fd8b84dc6e
Jin Yanan <jin_yan@worksap.co.jp>
2019-08-22 18:03:47 +0800

    datasource demo and multi datasource autowired demo

---
+++ spring02-multi-datasource-demo/src/main/java/com/anigy/data/spring02multidatasourcedemo/Spring02MultiDatasourceDemoApplication.java
@@ -0,0 +1,69 @@
+package com.anigy.data.spring02multidatasourcedemo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        JdbcTemplateAutoConfiguration.class })
+@Slf4j
+public class Spring02MultiDatasourceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring02MultiDatasourceDemoApplication.class, args);
+    }
+
+    @Bean
+    @ConfigurationProperties("foo.datasource")
+    public DataSourceProperties fooDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @Autowired
+    public DataSource fooDataSource(DataSourceProperties fooDataSourceProperties) {
+        log.info("foo datasource: {}", fooDataSourceProperties.getUrl());
+        return fooDataSourceProperties.initializeDataSourceBuilder().build();
+    }
+
+    @Bean
+    @Resource
+    public PlatformTransactionManager fooTxManager(DataSource fooDataSource) {
+        return new DataSourceTransactionManager(fooDataSource);
+    }
+
+    @Bean
+    @ConfigurationProperties("bar.datasource")
+    public DataSourceProperties barDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    public DataSource barDataSource() {
+        DataSourceProperties barDataSourceProperties = barDataSourceProperties();
+        log.info("bar datasource: {}", barDataSourceProperties.getUrl());
+        return barDataSourceProperties.initializeDataSourceBuilder().build();
+    }
+
+    @Bean
+    @Resource
+    public PlatformTransactionManager barTxManager(DataSource barDataSource) {
+        return new DataSourceTransactionManager(barDataSource);
+    }
+
+}
