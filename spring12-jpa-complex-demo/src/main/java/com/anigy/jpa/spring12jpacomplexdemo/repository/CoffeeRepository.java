e98bdb3998c24f1f025a9ecc0e023106d801702a
Jin Yanan <jin_yan@worksap.co.jp>
2019-09-02 18:01:42 +0800

    spring12-complex-jpa-demo

---
+++ spring12-jpa-complex-demo/src/main/java/com/anigy/jpa/spring12jpacomplexdemo/repository/CoffeeRepository.java
@@ -0,0 +1,6 @@
+package com.anigy.jpa.spring12jpacomplexdemo.repository;
+
+import com.anigy.jpa.spring12jpacomplexdemo.model.Coffee;
+
+public interface CoffeeRepository extends BaseRepository<Coffee, Long> {
+}
