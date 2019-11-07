efdc4cdae4a95b88e20ab3efcb5d519a71a8e000
Anigy92 <Anigy92@users.noreply.github.com>
2019-11-07 23:54:51 +0800

    mybatis-generator-demo

---
+++ spring14-mybatis-generator-demo/src/main/java/com/anigy/spring14mybatisgeneratordemo/Spring14MybatisGeneratorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.anigy.spring14mybatisgeneratordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring14MybatisGeneratorDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring14MybatisGeneratorDemoApplication.class, args);
+    }
+
+}
---
+++ spring14-mybatis-generator-demo/src/test/java/com/anigy/spring14mybatisgeneratordemo/Spring14MybatisGeneratorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anigy.spring14mybatisgeneratordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring14MybatisGeneratorDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
